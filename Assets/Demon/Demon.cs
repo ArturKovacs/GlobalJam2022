0b940d7f650a27869ce3e21a9bc37d8e0a6500bf
Artúr Kovács <kovacs.artur.barnabas@gmail.com>
2022-01-28 19:49:12 +0100

    Demon and Girl movement works

--- Assets/Demon/Demon.cs
+++ Assets/Demon/Demon.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Demon : MonoBehaviour
 {
+    public float Speed;
+    public bool IsContollerTarget;
+
+    public GameObject Platform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,30 @@ public class Demon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsContollerTarget)
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                PlacePlatform();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsContollerTarget)
+        {
+            float vertical = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");
+
+            var deltaPos = new Vector3(horizontal, vertical, 0) * Speed * Time.deltaTime;
+            gameObject.transform.Translate(deltaPos);
+        }
+    }
+    private void PlacePlatform()
+    {
+        var offset = new Vector3(0, 2, 0);
+        var platformPos = transform.position + offset;
+        Instantiate(Platform, platformPos, Quaternion.identity);
     }
 }
--- Assets/Girl/Girl.cs
+++ Assets/Girl/Girl.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Girl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    public float JumpStrength;
+    public float fastestReJumpTime;
+    public bool IsContollerTarget;
+
+    new Rigidbody2D rigidbody;
+
+    float lastJumpTime = 0;
+    bool wasJumpDown = true;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsContollerTarget)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+
+            var deltaPos = new Vector3(horizontal, 0, 0) * Speed * Time.deltaTime;
+            transform.Translate(deltaPos);
+
+            // Jump
+            bool isJumpDown = Input.GetButton("Submit");
+            var elapsedSinceLastJump = Time.time - lastJumpTime;
+            if (isJumpDown && !wasJumpDown && elapsedSinceLastJump > fastestReJumpTime)
+            {
+                rigidbody.AddForce(Vector3.up * JumpStrength, ForceMode2D.Impulse);
+                lastJumpTime = Time.time;
+            }
+
+            wasJumpDown = isJumpDown;
+        }
     }
 }
